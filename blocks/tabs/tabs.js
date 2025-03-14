1d40bfaaf691803958968cdd493cbeb4a1a7e3a7
alytv-thepixel <artem.lytvynenko@thepixel.com>
2025-03-14 15:49:40 +0200

    modelling update

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -18,7 +18,6 @@ import {
  * @returns {HTMLElement} The root element of the fragment
  */
 export async function loadFragment(path) {
-  console.log(path)
   if (path && path.startsWith('/')) {
     // eslint-disable-next-line no-param-reassign
     path = path.replace(/(\.plain)?\.html/, '');
@@ -44,17 +43,78 @@ export async function loadFragment(path) {
   return null;
 }
 
+function bind(block) {
+  let index = 0;
+  const tabs = block.querySelectorAll('.tab');
+  const contents = block.querySelectorAll('.content');
+
+  if (contents.length === 0) return;
+
+  function updateActiveSlide(newIndex) {
+    index = newIndex < 0 ? contents.length - 1 : newIndex % contents.length;
+
+    // Remove 'active' class from all slides & titles
+    contents.forEach((content, i) => content.classList.toggle('active', i === index));
+    tabs.forEach((tab, i) => tab.classList.toggle('active', i === index));
+  }
+
+  // Click on Title to Change Slide
+  block.addEventListener('click', (e) => {
+    const tab = e.target.closest('.tab');
+    if (!tab) return;
+
+    const ref = tab.getAttribute('data-ref');
+    if (!ref) return;
+
+    e.preventDefault();
+
+    const targetIndex = [...tabs].findIndex((t) => t.getAttribute('data-ref') === ref);
+    if (targetIndex !== -1) {
+      updateActiveSlide(targetIndex);
+    }
+  });
+}
 export default async function decorate(block) {
-  console.log(block)
-  // const link = block.querySelector('a');
-  // const path = link ? link.getAttribute('href') : block.textContent.trim();
-  // const fragment = await loadFragment(path);
-  // if (fragment) {
-  //   const fragmentSection = fragment.querySelector(':scope .section');
-  //   if (fragmentSection) {
-  //     block.classList.add(...fragmentSection.classList);
-  //     block.classList.remove('section');
-  //     block.replaceChildren(...fragmentSection.childNodes);
-  //   }
-  // }
+  let index = 1;
+  const tabs = document.createElement('div');
+  tabs.classList.add('tabs-wrap');
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const tabContent of [...block.children]) {
+    const [tab, contentHolder] = tabContent.children;
+    const tabId = `tab-${index}`;
+    const link = tabContent.querySelector('a');
+    const path = link ? link.getAttribute('href') : block.textContent.trim();
+    // eslint-disable-next-line no-await-in-loop
+    const fragment = await loadFragment(path);
+
+    tab.classList.add('tab');
+    tab.setAttribute('data-ref', tabId);
+
+    tabContent.setAttribute('id', tabId);
+    tabContent.className = 'content';
+    contentHolder.className = 'content-holder';
+
+    if (fragment) {
+      const fragmentSection = fragment.querySelector(':scope .section');
+      if (fragmentSection) {
+        contentHolder.classList.add(...fragmentSection.classList);
+        contentHolder.replaceChildren(...fragmentSection.childNodes);
+      }
+    }
+
+    if (index === 1) {
+      tab.classList.add('active');
+      tabContent.classList.add('active');
+    }
+
+    tabs.append(tab);
+    tabs.append(tabContent);
+    // eslint-disable-next-line no-plusplus
+    index++;
+  }
+
+  block.append(tabs);
+
+  bind(block);
 }
--- models/tabs.js
+++
@@ -1,3 +0,0 @@
-export default function Tabs({ children }) {
-  return `<div class="tabs">${children}</div>`;
-}
