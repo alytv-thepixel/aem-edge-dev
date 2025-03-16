5454fcf98261c75404399ab4f06f0df082e54ac9
alytv-thepixel <artem.lytvynenko@thepixel.com>
2025-03-16 17:57:29 +0200

    modelling update

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -134,7 +134,7 @@ export default async function decorate(block) {
 
   block.innerHTML = '';
 
-  console.log(block.closest('.section').hasAttribute(data-aue-resource))
+  console.log(block.closest('.section').hasAttribute('data-aue-resource'))
 
   if (isEditor) {
     if (ifMoreTabs) {
